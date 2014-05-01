093629fe779503f5fa3bd8791f8458cb8a55b356
darkpray <hindhra@aol.com>
2014-05-01 20:57:24 +0200

    Hello World & JavaDoc.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,12 @@
 
+/**
+ * TestProjekt
+ * @author Darkpray
+ *
+ */
 public class HelloWorld {
-
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World.");
 	}
 }
